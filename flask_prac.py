d4a1afbacae89604c0c06c83028945386abac087
stevedias796 <55200261+stevedias796@users.noreply.github.com>
2020-03-03 17:27:28 +0530

    Add files via upload

---
+++ flask_prac.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+import gmplot #for drawing maps and plotting 
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template("place_form.html")
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def searching():
+    if request.method == 'POST':
+        src_loc = request.form['src_loc']
+        des_loc = request.form['des_loc']
+        '''if (src_loc.len() == 0 or src_loc == "") and (des_loc.len() == 0 or des_loc == ""):
+            return HttpResponse("<p>Please Enter Source and Destination</p>")'''
+        src_loc = src_loc.replace(" ", "+")
+        des_loc = des_loc.replace(" ", "+")
+        src_lat_and_lon = []
+        des_lat_and_lon = []
+        lat = []
+        lon = []
+        get_lat_long(src_loc, src_lat_and_lon) #function to get lat and long of the location
+        lat.append(src_lat_and_lon[0])
+        lon.append(src_lat_and_lon[1])   
+
+        gmap = gmplot.GoogleMapPlotter(src_lat_and_lon[0], src_lat_and_lon[1], 15) #vikhroli
+
+        get_lat_long(des_loc, des_lat_and_lon) 
+        lat.append(des_lat_and_lon[0])
+        lon.append(des_lat_and_lon[1]) 
+        
+        gmap.scatter(lat, lon, '#faf600', size=40, marker=False)
+        gmap.plot(lat, lon, 'red', edge_with=9.5)
+
+        gmap.draw("D:\\Steve\\templates\\display_map.html")
+        return render_template("display_map.html")
+    else:
+        return render_template("place_form.html")  
+
+
+def get_lat_long(location, la_and_lo):
+    search_url = "https://www.google.com/search?q="+location+"+logitude+latitude"
+    #search_url = "https://www.google.co.in/maps/search/"+src_loc
+    print(search_url)
+    req = Request(search_url, headers={'User-Agent': 'Mozilla/5.0'})
+    web_byte = urlopen(req).read()
+    webpage = web_byte.decode() #to convert into utf-8
+    soup = BeautifulSoup(webpage,"lxml")
+
+    tags = soup.find_all('div', {'class': 'BNeawe iBp4i AP7Wnd'})
+    lat_long = tags[0].get_text()
+    lat_long_new = lat_long.split(",")
+
+    latitude = lat_long_new[0].split("°")
+    longitude = lat_long_new[1].split("°")
+
+    latitude = float(latitude[0])
+    longitude = float(longitude[0])
+    la_and_lo.append(latitude)
+    la_and_lo.append(longitude)
+    return
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
+from flask import Flask     
+app = Flask(__name__)   # Flask constructor 
+  
+# A decorator used to tells the application 
+# which URL is associated function 
+@app.route('/')       
+def hello(): 
+    return 'HELLO'
+  
+if __name__=='__main__': 
+   app.run()     
+   '''
